d3b52dc0400dca93b71983c63b44e72bf4858f88
dlrauddmlejrgn <dlrauddmlejrgn@gmail.com>
2023-02-23 00:18:12 +0900

    time notation

--- components/timeNotation.js
+++ components/timeNotation.js
@@ -1,6 +1,6 @@
 const timeNotation = (temp) => {
     var time;
-
+/*
     switch(temp[5]){
         case 'P': time='오후 '; break;
         default: time='오전 ';
@@ -8,6 +8,35 @@ const timeNotation = (temp) => {
     switch(temp[0]){
         case '0': time += temp.slice(1, 5); break;
         default: time += temp.slice(0, 5)
+    }
+*/
+
+    if (temp.length > 5){
+        switch(temp[5]){
+            case 'P': time='오후 '; break;
+            default: time='오전 ';
+        }
+        switch(temp[0]){
+            case '0': time += temp.slice(1, 5); break;
+            default: time += temp.slice(0, 5)
+        }
+    }
+    else 
+    {
+        var hh = Number(temp.slice(0, 2));
+        var mm = temp.slice(2);
+        if ( hh > 12 ) {
+            time = '오후 '+(hh-12)+mm;
+        }
+        else if ( hh == 12 ){ 
+            time = '오후 12'+mm;
+        }
+        else if ( hh > 0) {
+            time = '오전 '+hh+mm;
+        }
+        else {
+            time = '오전 12'+mm;
+        }
     }
 
     return time;
